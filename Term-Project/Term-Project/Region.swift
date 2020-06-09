3f8622c8f87c2ea7be85ea660608df8e96a13789
kosongji <32106405+kosongji@users.noreply.github.com>
2020-06-10 00:44:04 +0900

    지역별 검색
    지역별 검색 기능
    - 예시 항목 구현

---
+++ Term-Project/Term-Project/Region.swift
@@ -0,0 +1,18 @@
+//
+//  Region.swift
+//  Term-Project
+//
+//  Created by KPUGAME on 2020/06/10.
+//  Copyright © 2020 KPUGAME. All rights reserved.
+//
+
+import UIKit
+
+struct Region {
+    var name: String?
+    
+    init(name: String?)
+    {
+        self.name = name
+    }
+}
---
+++ Term-Project/Term-Project/RegionsData.swift
@@ -0,0 +1,44 @@
+//
+//  RegionsData.swift
+//  Term-Project
+//
+//  Created by KPUGAME on 2020/06/10.
+//  Copyright © 2020 KPUGAME. All rights reserved.
+//
+
+import Foundation
+
+let regionsData = [
+    Region(name:"가평군"),
+    Region(name:"고양시"),
+    Region(name:"과천시"),
+    Region(name:"광명시"),
+    Region(name:"광주시"),
+    Region(name:"구리시"),
+    Region(name:"군포시"),
+    Region(name:"김포시"),
+    Region(name:"남양주시"),
+    Region(name:"동두천시"),
+    Region(name:"부천시"),
+    Region(name:"성남시"),
+    Region(name:"수원시"),
+    Region(name:"시흥시"),
+    Region(name:"안산시"),
+    Region(name:"안성시"),
+    Region(name:"안양시"),
+    Region(name:"양주시"),
+    Region(name:"양평군"),
+    Region(name:"여주시"),
+    Region(name:"연천군"),
+    Region(name:"오산시"),
+    Region(name:"용인시"),
+    Region(name:"의왕시"),
+    Region(name:"의정부시"),
+    Region(name:"이천시"),
+    Region(name:"파주시"),
+    Region(name:"평택시"),
+    Region(name:"포천시"),
+    Region(name:"하남시"),
+    Region(name:"화성시")
+
+]
